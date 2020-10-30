aff83534b0489af2b3dec8385143a0d082be667e
Christian <christian@Oroszlan.Oroszlan>
2020-10-30 18:43:36 -0500

    roulette open endpoint

--- app.js
+++ app.js
@@ -2,31 +2,44 @@ const express = require('express');
 const app = express();
 const redisClient = require('./redis-client');
 const { v4: uuidv4 } = require('uuid');
-
 const PORT = process.env.PORT || 2112;
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
-    res.json({"name":"pepito"});
+    res.send('Welcome to the api Online Roulette');
 });
-
 app.get('/newRoulette', async function(req, res){
     let uuid = uuidv4();
-
     await redisClient.rpushAsync('allroulettes', uuid);
-    await redisClient.setAsync('roulettes', uuid);
-    await redisClient.setAsync(uuid, 1);
-    const newId = await redisClient.getAsync('roulettes');
-    return res.json({"id":newId});
+    let nameR='roulette#'+uuid;
+    await redisClient.hmsetAsync(nameR,
+    {
+        'id': uuid,
+        'status': 0,
+        'max': 10000,
+        'bets': ''
+    });
+    const newRoulette = await redisClient.hgetAllAsync('roulette#'+uuid);
+    return res.json(newRoulette.id);
 });
-
-app.post('/rouletteState', async function (req, res) {
+app.post('/rouletteOpen', async function (req, res) {
     let roulette = req.body.id;
-    let state= await redisClient.getAsync(roulette);
-    if(state) res.success=true;
-    else res.success=false;
-    return res.success;
+    roulette='roulette#'+roulette;
+    let currentRoulette = await redisClient.hgetAllAsync(roulette);
+    currentRoulette.status=1;
+    await redisClient.hmsetAsync(roulette,currentRoulette);
+    let newCurrentRoulette = await redisClient.hgetAllAsync(roulette);
+    if(newCurrentRoulette.status) {
+        res.success=true;
+        res.message="Roulette open correctly"
+    }
+    else {
+        res.success=false;
+        res.message="Roulette not open correctly"
+    }
+    return res.json(res.message);
 });
 
 app.get('/allRoulettes', async function(req, res){
